b3fa4cf26df948706c708d27260a0d71c82972ff
fire8327 <fire83274@gmail.com>
2023-01-31 01:40:38 +0300

    fix header

--- Assets/JS/custom.js
+++ Assets/JS/custom.js
@@ -1,12 +1,12 @@
 const header = document.getElementById("head");
 window.onscroll = function () {
 if (document.body.scrollTop > 48 || document.documentElement.scrollTop > 48) {
-  header.classList.remove("mt-6");
   header.classList.add("fixed");
+  header.classList.add("border-b");
   header.classList.add("bg-[#fdfbfe]");
 } else {
-  header.classList.add("mt-6");
   header.classList.remove("fixed");
+  header.classList.remove("border-b");
   header.classList.remove("bg-[#fdfbfe]");
 }
 }
