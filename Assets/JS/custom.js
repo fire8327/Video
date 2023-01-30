1116307fd7d6a555fc4db3da712a49d5131f6c1b
fire8327 <fire83274@gmail.com>
2023-01-31 01:34:48 +0300

    create header&footer

--- Assets/JS/custom.js
+++ Assets/JS/custom.js
@@ -1,25 +1,9 @@
-/* navigation */
-/*jshint esnext: true */
-const toggler = document.getElementById("toggler");
-const mNav = document.getElementById("mobile__nav");
-const close = document.getElementById("toggler__expanded");
-const overlay = document.getElementById("overlay");
-const body = document.getElementsByTagName("body")[0];
-
-toggler.addEventListener("click", () => {
-  mNav.classList.remove("translate-x-full");
-  overlay.classList.remove("hidden");
-  body.classList.add("overflow-hidden");
-});
-
-close.addEventListener("click", () => {
-  mNav.classList.add("translate-x-full");
-  overlay.classList.add("hidden");
-  body.classList.remove("overflow-hidden"); 
-});
-
-overlay.addEventListener("click", () => {
-  mNav.classList.add("translate-x-full");
-  overlay.classList.add("hidden"); 
-  body.classList.remove("overflow-hidden"); 
-});
+const head = document.getElementById("head");
+if (document.body.scrollTop > 48 || document.documentElement.scrollTop > 48) {
+  head.classList.add("fixed");
+  head.classList.remove("mt-6");
+  head.classList.add("bg-[#fdfbfe]");
+} else {
+  head.classList.remove("fixed");
+  head.classList.add("mt-6");
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,4 +5,4 @@ theme: {
 extend: {},
 },
 plugins: [],
-}]
+}
