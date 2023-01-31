197635567db1bf845ffd010de3a6a177b8cada59
fire8327 <114702513+fire8327@users.noreply.github.com>
2023-01-31 05:58:39 +0000

    header end

--- Assets/JS/custom.js
+++ Assets/JS/custom.js
@@ -1,12 +1,17 @@
 const header = document.getElementById("head");
+const number = document.getElementById("number");
 window.onscroll = function () {
-if (document.body.scrollTop > 48 || document.documentElement.scrollTop > 48) {
+if (document.body.scrollTop > 10 || document.documentElement.scrollTop > 10) {
   header.classList.add("fixed");
-  header.classList.add("border-b");
+  header.classList.add("border-b-2");
   header.classList.add("bg-[#fdfbfe]");
+  number.classList.remove("opacity-0");
+  number.classList.add("opacity-100");
 } else {
   header.classList.remove("fixed");
-  header.classList.remove("border-b");
+  header.classList.remove("border-b-2");
   header.classList.remove("bg-[#fdfbfe]");
+  number.classList.remove("opacity-100");
+  number.classList.add("opacity-0");
 }
 }
